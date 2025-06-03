b4f1bcb8298fe766f3a9678da5b94d1e82852e05
felimadu <fmadutsa@gmail.com>
2025-06-03 16:33:00 -0400

    adding gas token to GetOperationsToExecuteTransaction

--- typescript/ethers6-app/src/utils.ts
+++ typescript/ethers6-app/src/utils.ts
@@ -1,10 +1,4 @@
-import {
-  Account,
-  AccountCluster,
-  Activity,
-  ActivityStatus,
-  OnchainOperation,
-} from "@orb-labs/orby-core";
+import { Account, AccountCluster, Activity, ActivityStatus, OnchainOperation } from "@orb-labs/orby-core";
 import { ethers, TypedDataDomain, TypedDataField } from "ethers";
 import ERC20ABI from "./abi/erc20.json";
 import { OrbyProvider } from "@orb-labs/orby-ethers6";
@@ -22,18 +16,14 @@ export async function setup(
   const privateInstanceProvider = new OrbyProvider(orbyInstancePrivateUrl);
 
   // 2. Get address from private key
-  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "");
+  const wallet = new ethers.Wallet(process.env["PRIVATE_KEY"] ?? "");
   const address = wallet.address;
   console.log("\n[INFO] derived address from private key: ", address);
 
   // 3. Create account cluster
   console.log("\n[INFO] creating account cluster...");
-  const accounts = [
-    Account.toAccount({ vmType: "EVM", address, accountType: "EOA" }),
-  ];
-  const accountCluster = await privateInstanceProvider.createAccountCluster(
-    accounts
-  );
+  const accounts = [Account.toAccount({ vmType: "EVM", address, accountType: "EOA" })];
+  const accountCluster = await privateInstanceProvider.createAccountCluster(accounts);
   if (!accountCluster) {
     throw new Error("failed to create account cluster");
   }
@@ -44,18 +34,12 @@ export async function setup(
   console.log(`[INFO]   accountClusterId: ${accountClusterId}`);
   console.log(`[INFO]   chainId: ${chainId}`);
   console.log(`[INFO]   address: ${address}`);
-  const virtualNodeRpcUrl = await privateInstanceProvider.getVirtualNodeRpcUrl(
-    accountClusterId,
-    chainId,
-    address
-  );
+  const virtualNodeRpcUrl = await privateInstanceProvider.getVirtualNodeRpcUrl(accountClusterId, chainId, address);
 
   if (!virtualNodeRpcUrl) {
     throw new Error("failed to get virtual node RPC URL");
   } else {
-    console.log(
-      `[INFO] You can now use this URL to interact with the virtual node: ${virtualNodeRpcUrl}`
-    );
+    console.log(`[INFO] You can now use this URL to interact with the virtual node: ${virtualNodeRpcUrl}`);
   }
 
   // 5. Create virtual node client
@@ -67,10 +51,8 @@ export async function setup(
 // ******************************************** for signing *******************************************
 
 // TO-DO: create example that signs non-typed data
-export const signTransaction = async (
-  operation: OnchainOperation
-): Promise<string | undefined> => {
-  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "");
+export const signTransaction = async (operation: OnchainOperation): Promise<string | undefined> => {
+  const wallet = new ethers.Wallet(process.env["PRIVATE_KEY"] ?? "");
 
   console.log(`\n[INFO] Signing operation...`);
   console.log(`         Type: ${operation.type}`);
@@ -80,18 +62,7 @@ export const signTransaction = async (
   console.log(`         Chain ID: ${operation.chainId}`);
   console.log(`         TX RPC URL: ${operation.txRpcUrl}`);
 
-  const {
-    from,
-    to,
-    data,
-    gasPrice,
-    maxPriorityFeePerGas,
-    maxFeePerGas,
-    gasLimit,
-    value,
-    nonce,
-    chainId,
-  } = operation;
+  const { from, to, data, gasPrice, maxPriorityFeePerGas, maxFeePerGas, gasLimit, value, nonce, chainId } = operation;
 
   const txData = {
     from,
@@ -109,10 +80,8 @@ export const signTransaction = async (
   return await wallet.signTransaction(txData);
 };
 
-export const signTypedData = async (
-  operation: OnchainOperation
-): Promise<string | undefined> => {
-  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "");
+export const signTypedData = async (operation: OnchainOperation): Promise<string | undefined> => {
+  const wallet = new ethers.Wallet(process.env["PRIVATE_KEY"] ?? "");
 
   console.log(`\n[INFO] Signing operation...`);
   console.log(`         Type: ${operation.type}`);
@@ -129,36 +98,19 @@ export const signTypedData = async (
   };
   delete parsedData.types["EIP712Domain"];
 
-  return await wallet.signTypedData(
-    parsedData.domain,
-    parsedData.types,
-    parsedData.message
-  );
+  return await wallet.signTypedData(parsedData.domain, parsedData.types, parsedData.message);
 };
 
 // ******************************************** for getting operation set status *******************************************
 
-export const onOperationSetStatusUpdateCallback = async (
-  activity?: Activity
-): Promise<void> => {
-  console.log(
-    "[INFO] [onOperationSetStatusUpdateCallback] response: ",
-    activity
-  );
+export const onOperationSetStatusUpdateCallback = async (activity?: Activity): Promise<void> => {
+  console.log("[INFO] [onOperationSetStatusUpdateCallback] response: ", activity);
 
   if (!activity) {
     // handle not found
-  } else if (
-    [ActivityStatus.SUCCESSFUL, ActivityStatus.PENDING].includes(
-      activity?.overallStatus
-    )
-  ) {
+  } else if ([ActivityStatus.SUCCESSFUL, ActivityStatus.PENDING].includes(activity?.overallStatus)) {
     // handle success
-  } else if (
-    [ActivityStatus.FAILED, ActivityStatus.NOT_FOUND].includes(
-      activity?.overallStatus
-    )
-  ) {
+  } else if ([ActivityStatus.FAILED, ActivityStatus.NOT_FOUND].includes(activity?.overallStatus)) {
     // handle other cases
   }
 };
